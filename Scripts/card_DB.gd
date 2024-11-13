3737bdad10aadefb13e464650f8c50e21e24027b
Christopher Stuart <63csmith@gmail.com>
2024-11-12 18:16:16 -0700

    update card_DB

--- Scripts/card_DB.gd
+++ Scripts/card_DB.gd
@@ -1,5 +1,69 @@
 const CARDS = {
-	"1" : [1,1],
-	"2" : [2,2],
-	"3" : [3,3]
-}
+	"2_of_spades": [2],        # 🂢 = 2️⃣ of Spades
+	"2_of_hearts": [2],        # 🂢 = 2️⃣ of Hearts
+	"2_of_diamonds": [2],      # 🂢 = 2️⃣ of Diamonds
+	"2_of_clubs": [2],         # 🂢 = 2️⃣ of Clubs
+
+	"3_of_spades": [3],        # 🂣 = 3️⃣ of Spades
+	"3_of_hearts": [3],        # 🂣 = 3️⃣ of Hearts
+	"3_of_diamonds": [3],      # 🂣 = 3️⃣ of Diamonds
+	"3_of_clubs": [3],         # 🂣 = 3️⃣ of Clubs
+
+	"4_of_spades": [4],        # 🂤 = 4️⃣ of Spades
+	"4_of_hearts": [4],        # 🂤 = 4️⃣ of Hearts
+	"4_of_diamonds": [4],      # 🂤 = 4️⃣ of Diamonds
+	"4_of_clubs": [4],         # 🂤 = 4️⃣ of Clubs
+
+	"5_of_spades": [5],        # 🂥 = 5️⃣ of Spades
+	"5_of_hearts": [5],        # 🂥 = 5️⃣ of Hearts
+	"5_of_diamonds": [5],      # 🂥 = 5️⃣ of Diamonds
+	"5_of_clubs": [5],         # 🂥 = 5️⃣ of Clubs
+
+	"6_of_spades": [6],        # 🂦 = 6️⃣ of Spades
+	"6_of_hearts": [6],        # 🂦 = 6️⃣ of Hearts
+	"6_of_diamonds": [6],      # 🂦 = 6️⃣ of Diamonds
+	"6_of_clubs": [6],         # 🂦 = 6️⃣ of Clubs
+
+	"7_of_spades": [7],        # 🂧 = 7️⃣ of Spades
+	"7_of_hearts": [7],        # 🂧 = 7️⃣ of Hearts
+	"7_of_diamonds": [7],      # 🂧 = 7️⃣ of Diamonds
+	"7_of_clubs": [7],         # 🂧 = 7️⃣ of Clubs
+
+	"8_of_spades": [8],        # 🂨 = 8️⃣ of Spades
+	"8_of_hearts": [8],        # 🂨 = 8️⃣ of Hearts
+	"8_of_diamonds": [8],      # 🂨 = 8️⃣ of Diamonds
+	"8_of_clubs": [8],         # 🂨 = 8️⃣ of Clubs
+
+	"9_of_spades": [9],        # 🂩 = 9️⃣ of Spades
+	"9_of_hearts": [9],        # 🂩 = 9️⃣ of Hearts
+	"9_of_diamonds": [9],      # 🂩 = 9️⃣ of Diamonds
+	"9_of_clubs": [9],         # 🂩 = 9️⃣ of Clubs
+
+	"10_of_spades": [10],      # 🂪 = 🔟 of Spades
+	"10_of_hearts": [10],      # 🂪 = 🔟 of Hearts
+	"10_of_diamonds": [10],    # 🂪 = 🔟 of Diamonds
+	"10_of_clubs": [10],       # 🂪 = 🔟 of Clubs
+
+	"jack_of_spades": [11],    # 🂫 = Jack (11) of Spades
+	"jack_of_hearts": [11],    # 🂫 = Jack (11) of Hearts
+	"jack_of_diamonds": [11],  # 🂫 = Jack (11) of Diamonds
+	"jack_of_clubs": [11],     # 🂫 = Jack (11) of Clubs
+
+	"queen_of_spades": [12],   # 🂭 = Queen 👸 (12) of Spades
+	"queen_of_hearts": [12],   # 🂭 = Queen 👸 (12) of Hearts
+	"queen_of_diamonds": [12], # 🂭 = Queen 👸 (12) of Diamonds
+	"queen_of_clubs": [12],    # 🂭 = Queen 👸 (12) of Clubs
+
+	"king_of_spades": [13],    # 🂮 = King 🤴 (13) of Spades
+	"king_of_hearts": [13],    # 🂮 = King 🤴 (13) of Hearts
+	"king_of_diamonds": [13],  # 🂮 = King 🤴 (13) of Diamonds
+	"king_of_clubs": [13],     # 🂮 = King 🤴 (13) of Clubs
+
+	"ace_of_spades": [14],     # 🂡 = Ace 🅰️ (14) of Spades
+	"ace_of_hearts": [14],     # 🂡 = Ace 🅰️ (14) of Hearts
+	"ace_of_diamonds": [14],   # 🂡 = Ace 🅰️ (14) of Diamonds
+	"ace_of_clubs": [14],      # 🂡 = Ace 🅰️ (14) of Clubs
+
+	"joker_1": ["win"],        # Joker Power Card 🃏 = Win the hand
+	"joker_2": ["win"]           
+};
